96aea9a67c90fe48641e0de7a0197f4d0319fc1e
eraldvelcani <eraldvelcani2003@gmail.com>
2025-12-29 03:19:48 +0100

    introduced contextAPI to Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,19 +1,31 @@
 import { Outlet } from "react-router-dom";
 import { MobileBar, DesktopBar } from "../components";
 import Wrapper from "../assets/wrappers/Dashboard";
+import { createContext, useContext, useState } from "react";
+
+const DashboardContext = createContext(); //context step 1: creating context to use toggleSidebar in props 
 
 const Dashboard = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+    
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
+
     return (
-        <Wrapper>
-            <main className="dashboard">
-                <MobileBar />
-                <DesktopBar />
-                <div className="dashboard-page">
-                    <Outlet />
-                </div>
-            </main>
-        </Wrapper>
+        <DashboardContext.Provider value={ { showSidebar } }> {/* context step 2: now all props below can use this value */}
+            <Wrapper>
+                <main className="dashboard">
+                    <MobileBar />
+                    <DesktopBar />
+                    <div className="dashboard-page">
+                        <Outlet />
+                    </div>
+                </main>
+            </Wrapper>
+        </DashboardContext.Provider>
     );
 };
 
+export const useDashboardContext = () => useContext(DashboardContext) //context step 3: export
 export default Dashboard;
