2ce625b989ad8e822c2254b68684505457f68805
eraldvelcani <eraldvelcani2003@gmail.com>
2025-12-30 06:23:19 +0100

    added DesktopBar.js component

--- client/src/components/DesktopBar.jsx
+++ client/src/components/DesktopBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import NavLinks from './NavLinks';
+import Logo from './Logo';
+import { useDashboardContext } from '../pages/Dashboard';
 
 const DesktopBar = () => {
+  const {showSidebar} = useDashboardContext();
+
   return (
-    <div>DesktopBar</div>
+    <div>
+        <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+            <div className='content'>
+                <header>
+                    <Logo />
+                </header>
+                <NavLinks isDesktopBar />
+            </div>
+        </div>
+    </div>
   )
 }
 
--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -3,14 +3,14 @@ import links from "../utils/links";
 import { NavLink } from "react-router-dom";
 import { useDashboardContext } from "../pages/Dashboard";
 
-const NavLinks = () => {
+const NavLinks = ({ isDesktopBar }) => {
   const {toggleSidebar} = useDashboardContext();  
   return (
     <div className="nav-links">
       {links.map((link) => {
         const { text, path /*, icon */ } = link;
         return (
-          <NavLink to={path} key={text} className="nav-link">
+          <NavLink to={path} key={text} className="nav-link" onClick={isDesktopBar ? null : toggleSidebar}>
             <span className="icon">{/*{icon}*/}</span>
             {text}
           </NavLink>
