d106032760107d94ac3110fc9b528a3a88fcba79
eraldvelcani <eraldvelcani2003@gmail.com>
2026-01-01 08:57:21 +0100

    get posts and create post functionality, currently testing with postman

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 import * as dotenv from 'dotenv'; dotenv.config();
-import express from "express"; const app = express();
+import express, { json } from "express"; const app = express();
 import morgan from "morgan";
+import { nanoid } from 'nanoid';
 
+let posts = [
+    {
+        id:nanoid(), user: 'user1', content: 'media'
+    },
+    {
+        id:nanoid(), user: 'user2', content: 'text'
+    },
+    {
+        id:nanoid(), user: 'user3', content: 'text'
+    },
+]
 
 if (process.env.NODE_ENV === 'development' ) {
     app.use(morgan('dev'));
 }
+
 app.use(express.json()); //middleware to accept json data coming from frontend e.g user logging in, creating post etc
 
-app.get('/', (req, res) => {
-    res.send('hi hi');
+//get all posts
+app.get('/api/v1/posts', (req, res) => {
+    res.status(200).json({ posts });
 });
 
-app.post('/', (req, res) => {
-    console.log(res);
-    res.json({message: "data received", data: req.body});
-})
+//create post
+app.post('/api/v1/post', (req, res) => {
+    const {user, content} = req.body;
+    if (!user || !content) {
+        return res.status(400).json({ msg: "error: user or content not provided" });
+    }
+    const id = nanoid(8);
+    const post = {id, user, content};
+    posts.push(post);
+    res.status(200).json({ post });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
